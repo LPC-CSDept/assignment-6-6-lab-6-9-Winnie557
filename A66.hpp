83de30c65e5efcca41dfe215d12a561819423637
Winnie557 <110578029+Winnie557@users.noreply.github.com>
2022-10-06 07:16:52 +0000

    sign

--- A66.hpp
+++ A66.hpp
@@ -11,16 +11,16 @@ void getinput(int &num1, int &num2, int &num3){
     cin >> num1 >> num2 >> num3;
 }
 void maxmin(int n1, int n2, int n3, int &max, int &min){
-    if(n1 >= n2 && n1 >= n3)
+    if(n1 > n2 && n1 > n3)
     max = n1;
-    if(n2 >= n1 && n2 >= n3)
+    if(n2 > n1 && n2 > n3)
     max = n2;
-    if(n3 >= n1 && n3 >= n2)
+    if(n3 > n1 && n3 > n2)
     max = n3;
-    if(n1 <= n2 && n1 <= n3)
+    if(n1 < n2 && n1 < n3)
     min = n1;
-    if(n2 <=n1 && n2<= n3)
+    if(n2 < n1 && n2< n3)
     min = n2;
-    else if(n3 <= n1 && n3 <= n2)
+    else if(n3 < n1 && n3 < n2)
     min = n3;
 }
